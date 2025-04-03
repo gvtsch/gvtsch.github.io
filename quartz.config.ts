3d5646306cad8599bc9330dfd01dcb14f8ba7f1b
Christoph Kempkes <88708549+gvtsch@users.noreply.github.com>
2025-04-03 21:21:20 +0200

    Aktualisieren von quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Christoph Kempkes",
+    pageTitle: "Digitaler Garten",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
