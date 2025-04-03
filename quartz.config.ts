9d7e18f85d3c35fd419c17cce8f332d899d76495
Christoph Kempkes <88708549+gvtsch@users.noreply.github.com>
2025-04-03 21:26:31 +0200

    Aktualisieren von quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -56,6 +56,7 @@ const config: QuartzConfig = {
   },
   plugins: {
     transformers: [
+      Plugin.CreatedModifiedDate(),
       Plugin.FrontMatter(),
       Plugin.CreatedModifiedDate({
         priority: ["frontmatter", "git", "filesystem"],
